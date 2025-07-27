e8e3d4f66ae2ba638e9327b371a489cdc41b8281
harshadajain07 <harshadajain3007@gmail.com>
2025-07-27 12:06:44 +0530

    Added new file

--- basics/HelloWorld.js
+++ basics/HelloWorld.js
@@ -1,1 +1,20 @@
 console.log("Hello World!!");
+a = 10;
+b = 20;
+c = a + b;
+console.log("Sum of a and b is: " + c);
+console.log("This is a simple JavaScript program.");
+console.log("End of program.");
+console.log("Goodbye!");
+console.log("This is a basic example of JavaScript code.");
+console.log("Let's add some more functionality.");
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("Alice");
+greet("Bob");
+console.log("Functionality added successfully.");
+console.log("This is a demonstration of console logging.");
+console.log("You can run this code in any JavaScript environment.");
+console.log("Make sure to check the console for output.");
+console.log("This is a simple addition operation.");
