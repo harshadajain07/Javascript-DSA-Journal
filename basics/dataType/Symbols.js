e59095d5be7fca958f54fbd0631421da8c08ab8b
harshadajain07 <harshadajain3007@gmail.com>
2025-08-09 14:02:04 +0530

    Added Symbols.js file

--- basics/dataType/Symbols.js
+++ basics/dataType/Symbols.js
@@ -2,13 +2,39 @@ const mySymbol = Symbol();
 console.log(typeof mySymbol);
 console.log(mySymbol);
 
-const namedSymbol = Symbol("name");
+const namedSymbol = Symbol("monkey");
 console.log(typeof namedSymbol);
 console.log(namedSymbol);
 
 console.log(mySymbol === namedSymbol);
-console.log(Symbol("name") === namedSymbol);
+console.log(Symbol("monkey") === namedSymbol);
 
 console.log(mySymbol === mySymbol);
 console.log(mySymbol.description);
 console.log(namedSymbol.description);
+
+const characters = {
+  name: "Money Heist",
+  class: "Axe Riskey",
+  quote: "I'll never let you down",
+  __admin__: false,
+};
+// console.log(characters.name);
+// console.log(characters.class);
+// console.log(characters.quote);
+// console.log(characters.__admin__);
+
+//Return the string indices of "0","1","2","3"
+for (const key in Object.keys(characters)) {
+  console.log(key);
+}
+
+//Return actual key name of object
+for (const key of Object.keys(characters)) {
+  console.log(key);
+}
+
+const keys = Object.keys(characters);
+for (idx in keys) {
+  console.log(keys[idx]);
+}
